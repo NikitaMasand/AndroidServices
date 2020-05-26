ef36646bf493169f483e50c2b428fb3e4218853c
NikitaMasand <masandnikita@gmail.com>
2020-05-26 15:34:17 +0530

    create bind service and bound service to activity

--- app/src/main/java/com/example/androidServices/MainActivity.java
+++ app/src/main/java/com/example/androidServices/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.androidServices;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,8 +11,7 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
-
-import com.example.androidServices.R;
+import com.example.androidServices.services.MusicPlayerService;
 import com.example.androidServices.services.MyDownloadService;
 
 
@@ -19,6 +22,31 @@ public class MainActivity extends AppCompatActivity {
     private TextView mLog;
     private ProgressBar mProgressBar;
     public static final String MESSAGE_KEY = "message_key";
+    private MusicPlayerService mMusicPlayerService;
+    private ServiceConnection mServiceConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder iBinder) {
+            MusicPlayerService.MyServiceBinder myServiceBinder = (MusicPlayerService.MyServiceBinder) iBinder;
+            mMusicPlayerService = myServiceBinder.getService();
+            mBound = true;
+            Log.d(TAG, "onServiceConnected: ");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+            Log.d(TAG, "onServiceDisconnected: ");
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(MainActivity.this, MusicPlayerService.class);
+        bindService(intent,mServiceConn, Context.BIND_AUTO_CREATE);
+    }
+
+    private boolean mBound = false;
 
     private void initViews() {
         mScroll = (ScrollView) findViewById(R.id.scrollLog);
@@ -32,23 +60,35 @@ public class MainActivity extends AppCompatActivity {
         initViews();
 
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mBound) {
+            unbindService(mServiceConn);
+            mBound = false;
+        }
+    }
+
     public void runCode (View view) {
-        log("Running code");
+        log(mMusicPlayerService.getValue());
         displayProgressBar(true);
 
         //send intent to download service
-        for(String song : Playlist.songs) {
-            Intent intent = new Intent(MainActivity.this, MyDownloadService.class);
-            intent.putExtra(MESSAGE_KEY, song);
-            startService(intent);
-        }
+        //startservice:
+//
+//        for(String song : Playlist.songs) {
+//            Intent intent = new Intent(MainActivity.this, MyDownloadService.class);
+//            intent.putExtra(MESSAGE_KEY, song);
+//            startService(intent);
+//        }
         }
 
 
 
     public void clearOutput(View v) {
-        Intent intent = new Intent(MainActivity.this, MyDownloadService.class);
-        stopService(intent);
+//        Intent intent = new Intent(MainActivity.this, MyDownloadService.class);
+//        stopService(intent);
         mLog.setText("");
         scrollTextToEnd();
     }
